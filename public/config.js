231ba022f70f91f74e6f95b6c2eed32aca7f83f1
thedrummeredi62-create <thedrummeredi62@gmail.com>
2026-09-23 01:45:04 -0300

    Enable production launch layer

--- public/config.js
+++ public/config.js
@@ -2,3 +2,13 @@ window.PERMUTA_CONFIG = {
   SUPABASE_URL: "https://dayspznngsgymwugkjjv.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_t6GOPNwDOPNbPIZooLnNfw_6a2w8QRU"
 };
+/* Production launch layer */
+(function(){
+  var s=document.createElement('script');
+  s.src='launch.js?v=20260923-1';
+  s.defer=true;
+  document.head.appendChild(s);
+  var m=document.createElement('link');
+  m.rel='manifest';m.href='manifest.webmanifest';
+  document.head.appendChild(m);
+})();
